6b9b55a039a2fd7ef69c6117796a757db1bc3c2b
Fernando Frazão <ffrazao@users.noreply.github.com>
2015-07-29 15:32:43 -0300

    projeto de contratos

---
+++ contrato/app.js
@@ -0,0 +1,23 @@
+angular.module('contrato', ['ui.bootstrap','ui.utils','ui.router','ngAnimate']);
+
+angular.module('contrato').config(function($stateProvider, $urlRouterProvider) {
+
+    /* Add New States Above */
+    $urlRouterProvider.otherwise('/home');
+
+});
+
+angular.module('contrato').run(function($rootScope) {
+
+    $rootScope.safeApply = function(fn) {
+        var phase = $rootScope.$$phase;
+        if (phase === '$apply' || phase === '$digest') {
+            if (fn && (typeof(fn) === 'function')) {
+                fn();
+            }
+        } else {
+            this.$apply(fn);
+        }
+    };
+
+});
