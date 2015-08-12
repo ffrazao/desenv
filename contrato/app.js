e1015bcabcf0c1d2b464424a09d78c5411aeb974
Fernando Frazão <ffrazao@users.noreply.github.com>
2015-08-11 22:44:58 -0300

    Inclusao do componente de endereco

--- contrato/app.js
+++ contrato/app.js
@@ -1,5 +1,5 @@
 angular.module('principal', ['ui.bootstrap', 'ui.utils', 'ui.router', 'ngAnimate', 'toastr', 'sticky', 'ui.mask', 'ui.utils.masks', 'flow',
-  'painel.vidro', 'frz.navegador', 'casa', 'contrato']);
+  'painel.vidro', 'endereco', 'frz.navegador', 'casa', 'contrato']);
 
 // inicio: codigo para habilitar o modal recursivo
 angular.module('principal').factory('$modalInstance', function () {
